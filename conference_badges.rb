3728fd76a5ed700d39122793ed3d1feff11b8a1b
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-20 19:42:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,6 @@ end
 
 def assign_rooms(speakers)
   
-  speakers.map do |name,index|
+  speakers.map.with_index do |name,index|
     "Hello, #{name}! You'll be assigned to room #{index + 1}!"
 end
