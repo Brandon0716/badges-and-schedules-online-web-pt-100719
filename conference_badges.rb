c8ea5a02ab70646900f3abfc17afa88724d8f349
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-20 19:23:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 def batch_badge_creator(names)
   new_array = []
   names.each do |name|
-    new_array.push(badge_maker(names))
+    new_array.push(badge_maker(name))
   end
   return new_array
 end
